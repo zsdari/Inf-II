0f0ec48abae4506cbdc606270950fc707db57a7c
zsdari <dmarton434@gmail.com>
2026-03-31 09:56:41 +0200

    frisstés

---
+++ PythonProject/prog0217.py
@@ -0,0 +1,15 @@
+def menut_kiir(tipus_1, tipus_2):
+    '''
+     menü kiírása
+    '''
+    print(f"Itt van két paraméter: {tipus_1},{tipus_2}")
+
+menut_kiir(tipus_1 = "cápa", tipus_2 = "tigris")
+
+def osszeadas (x,y):
+    eredmeny = x+y
+    print(f'Az összeg: {eredmeny}')
+
+a = 5
+b = 10
+osszeadas(a,b)
---
+++ PythonProject/prog0217ossocketsystem.py
@@ -0,0 +1,8 @@
+import os
+import socket
+import platform
+import sys
+
+print(platform.system())
+print(socket.gethostname())
+print(sys.version)
